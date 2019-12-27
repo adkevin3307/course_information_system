009eda3bffec616920a9d234f0a3a16721b32243
charles <egg99014@gmail.com>
2019-12-27 15:35:31 +0800

    add message block and update show data in course.js

--- assets/js/course.js
+++ assets/js/course.js
@@ -5,6 +5,16 @@ this js is used to get course's all information by the course_id and grade(gived
 var course_infos; // store courses information we get from DB
 var messages;
 
+// check if there is data and set data in web
+function setData(id, value) {
+    if(value == null) {
+        $(id).text("");
+    }
+    else {
+        $(id).text(value);
+    }
+}
+
 // get information from database and display on web
 $(document).ready(function(){
     // process input from url
@@ -27,26 +37,26 @@ $(document).ready(function(){
     var course_info = course_infos[0]; // turn array of course into a course
 
     // set course data into web page
-    $("#course_name_en").text(course_info.eng_course_name);
-    $("#course_name_ch").text(course_info.chi_course_name);
+    setData("#course_name_en", course_info.eng_course_name);
+    setData("#course_name_ch", course_info.chi_course_name);
     // basic_info
-    $("#course_id").text(course_info.course_id);
-    $("#faculty_name").text(course_info.faculty_name);
-    $("#professor").text(course_info.professor);
-    $("#grade").text(course_info.grade);
-    $("#credit").text(course_info.credit);
-    $("#hour").text(course_info.hour);
-    $("#max_students").text(course_info.max_students);
-    $("#min_students").text(course_info.min_students);
-    $("#students").text(course_info.students + "人 (此人數為最近一次查詢時人數)");
+    setData("#course_id", course_info.course_id);
+    setData("#faculty_name", course_info.faculty_name);
+    setData("#professor", course_info.professor);
+    setData("#grade", course_info.grade);
+    setData("#credit", course_info.credit);
+    setData("#hour", course_info.hour);
+    setData("#max_students", course_info.max_students);
+    setData("#min_students", course_info.min_students);
+    setData("#students", course_info.students + "人 (此人數為最近一次查詢時人數)");
     if(course_info.internship) {
         $("#internship").text("是");
     }
     else {
         $("#internship").text("否");
     }
-    $("#category").text(course_info.category);
-    $("#duration").text(course_info.duration);
+    setData("#category", course_info.category);
+    setData("#duration", course_info.duration);
     // schedule and classroom
     var schedule_content = "";
     var classroom_content = "";
@@ -58,12 +68,12 @@ $(document).ready(function(){
             classroom_content += ",";
         }
     }
-    $("#class_schedule").text(schedule_content);
-    $("#classroom").text(classroom_content);
+    setData("#class_schedule", schedule_content);
+    setData("#classroom", classroom_content);
 
-    $("#main_field").text(course_info.main_field);
-    $("#sub_field").text(course_info.sub_field);
-    $("#description").text(course_info.description);
+    setData("#main_field", course_info.main_field);
+    setData("#sub_field", course_info.sub_field);
+    setData("#description", course_info.description);
     // co_professors
     var co_professors_content = "";
     for (let i = 0; i < course_info.co_professors.length; i++) {
@@ -72,39 +82,45 @@ $(document).ready(function(){
             co_professors_content += ",";
         }
     }
-    $("#co_professors").text(co_professors_content);
+    setData("#co_professors", co_professors_content);
 
     // syllabus
-    $("#chi_objective").text(course_info.chi_objective);
-    $("#eng_objective").text(course_info.eng_objective);
-    $("#chi_pre_course").text(course_info.chi_pre_course);
-    $("#eng_pre_course").text(course_info.eng_pre_course);
-    $("#chi_outline").text(course_info.chi_outline);
-    $("#eng_outline").text(course_info.eng_outline);
-    $("#chi_teaching_method").text(course_info.chi_teaching_method);
-    $("#eng_teaching_method").text(course_info.eng_teaching_method);
-    $("#chi_reference").text(course_info.chi_reference);
-    $("#eng_reference").text(course_info.eng_reference);
-    $("#chi_syllabus").text(course_info.chi_syllabus);
-    $("#eng_syllabus").text(course_info.eng_syllabus);
-    $("#chi_evaluation").text(course_info.chi_evaluation);
-    $("#eng_evaluation").text(course_info.eng_evaluation);
+    setData("#chi_objective", course_info.chi_objective);
+    setData("#eng_objective", course_info.eng_objective);
+    setData("#chi_pre_course", course_info.chi_pre_course);
+    setData("#eng_pre_course", course_info.eng_pre_course);
+    setData("#chi_outline", course_info.chi_outline);
+    setData("#eng_outline", course_info.eng_outline);
+    setData("#chi_teaching_method", course_info.chi_teaching_method);
+    setData("#eng_teaching_method", course_info.eng_teaching_method);
+    setData("#chi_reference", course_info.chi_reference);
+    setData("#eng_reference", course_info.eng_reference);
+    setData("#chi_syllabus", course_info.chi_syllabus);
+    setData("#eng_syllabus", course_info.eng_syllabus);
+    setData("#chi_evaluation", course_info.chi_evaluation);
+    setData("#eng_evaluation", course_info.eng_evaluation);
 
     // get message and put in messages
-    /*$.ajax({
+    $.ajax({
         url: "/api/messages/" + course_id,
         type: "GET",
-        cache: false,
         success: function(response){
             messages = response;
         }
     });
 
-    // set old messages into web page
-    var content = "";
-    for (let i = 0; i < messages.length; i++) {
-        content += "<tr><td>";
-        content += messages[i];
-        content += "</td></tr>";
-    }*/
+    for(let i = 0; i < messages.length; i++) {
+		$('#outputclass').append(
+			"<div>"+
+				"<div class='box'>"+
+					"<div class='content'>"+
+						"<header class='align-center'>"+
+							"<p>" + messages[i].content + "</p>"+
+						"</header>"+
+					"</div>"+
+				"</div>"+
+			"</div>"
+		);
+	
+	}
 });
